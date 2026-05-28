7c7ac7c3f618121fb268e4cbc7bc84a711426833
Harsh Kumar <harshj7.net@gmail.com>
2026-05-29 01:47:58 +0530

    feat: update Redis client implementation and versioning in package files

--- src/redis.ts
+++ src/redis.ts
@@ -1,20 +1,70 @@
-import 'server-only'
-
 import { createClient } from 'redis'
 
-export const redis = createClient({
-    socket: {
-        host: process.env.REDIS_HOST || '127.0.0.1',
-        port: Number(process.env.REDIS_PORT || 6379),
-    },
+let client: ReturnType<
+    typeof createClient
+> | null = null
 
-    ...(process.env.REDIS_PASSWORD
-        ? {
-            password: process.env.REDIS_PASSWORD,
+const getClient = async () => {
+    if (!client) {
+        client = createClient({
+            socket: {
+                host:
+                    process.env.REDIS_HOST ||
+                    '127.0.0.1',
+
+                port: Number(
+                    process.env.REDIS_PORT || 6379
+                ),
+            },
+
+            ...(process.env.REDIS_PASSWORD
+                ? {
+                    password:
+                        process.env.REDIS_PASSWORD,
+                }
+                : {}),
+        })
+
+        client.on('error', (err) => {
+            console.error(
+                'Redis Error:',
+                err
+            )
+        })
+
+        if (!client.isOpen) {
+            await client.connect()
         }
-        : {}),
-})
+    }
 
-redis.connect()
+    return client
+}
+
+export const redis = new Proxy(
+    {},
+    {
+        get(_, prop) {
+            return async (...args: any[]) => {
+                const redis =
+                    await getClient()
+
+                const method =
+                    redis[
+                    prop as keyof typeof redis
+                    ]
+
+                if (
+                    typeof method === 'function'
+                ) {
+                    return (
+                        method as any
+                    ).apply(redis, args)
+                }
+
+                return method
+            }
+        },
+    }
+)
 
 export default redis
